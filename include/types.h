2d4c26663b8b014233b9d7446f69d972d64bd949
LatorreEngineering <raul.latorre@gmail.com>
2025-10-30 15:28:10 +0100

    Add types.h with standard and CAN type definitions

---
+++ include/types.h
@@ -0,0 +1,15 @@
+#ifndef TYPES_H
+#define TYPES_H
+
+#include <stdint.h>
+
+// Standard return type
+typedef uint8_t Std_ReturnType;
+#define E_OK 0
+#define E_NOT_OK 1
+
+// CAN types
+typedef uint32_t CanIdType;
+typedef uint8_t CanDataType;
+
+#endif
