642e85db8c00422f49b46d720ec90da230ee4730
LatorreEngineering <raul.latorre@gmail.com>
2025-10-30 15:28:35 +0100

    Add configuration header file for project settings

---
+++ include/config.h
@@ -0,0 +1,13 @@
+#ifndef CONFIG_H
+#define CONFIG_H
+
+// Project configuration
+
+// CAN configuration
+#define CAN_MAX_DATA_LENGTH 8
+#define CAN_NODE_ID 0x01
+
+// Temperature SWC config
+#define TEMPERATURE_SENSOR_ID 0x100
+
+#endif
